63bc9870353931a7aa5ffbd9dffba601a3dbea74
najmabboud <152595898+najmabboud@users.noreply.github.com>
2024-04-25 19:47:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -214,17 +214,44 @@ def child():
 def eaa():
     if request.method == 'POST':
         
-        ak=request.form['ak']
-        if ak=='نعم':
+        ak1=request.form['ak']
+        if ak1=='نعم':
             ea.append('1')
         
         
     return render_template('eaa.html')
 
 
+@app.route('/state',methods=['GET', 'POST'])  
+def state():
+    if request.method == 'POST':
+        
+        st=request.form['st1']
+        if st=='سكان أصليين':
+            sa.append('1')
+        
+        elif st=='عائدين':
+            aa.append('1')
+        elif st=='نازحين':
+            na.append('1')
+    return render_template('state.html')
 
 
 
+
+
+@app.route('/hamo',methods=['GET', 'POST'])  
+def hamo():
+    if request.method == 'POST':
+        
+        p11=request.form['p1']
+        if p11=='حامل':
+            ha.append('1')
+        elif p11=='مرضع':
+            mo.append('1')
+        
+        
+    return render_template('hamo.html')
 
 
 
@@ -238,8 +265,14 @@ def display():
     # username = request.args.get('username')
     # password = request.args.get('password')
     # هنا يمكنك استخدام المعلومات المستلمة لعرضها في صفحة HTML
-
-    return render_template('display.html', malecount=len(malecount),nummalemin6month=len(nummalemin6month) )
+    
+    
+    return render_template('display.html',sar=len(sa),nar=len(na),aar=len(aa),malecountr=len(malecount),nummalemin6monthr=len(nummalemin6month),nummalebet612monthr=len(nummalebet612month),nummalebet1324monthr=len(nummalebet1324month),nummalebet2559monthr=len(nummalebet2559month),
+    nummalebet518yearr=len(nummalebet518year),nummaleover18r=len(nummaleover18),fmalecountr=len(femalecount),
+    numfmalemin6monthr=len(numfmalemin6month),numfmalebet612monthr=len(numfmalebet612month),
+    numfmalebet1324monthr=len(numfmalebet1324month),numfmalebet2559monthr=len(numfmalebet2559month),
+    numfmalebet518yearr=len(numfmalebet518year),numfmaleover18r=len(numfmaleover18),m1r=len(m1), m2r=len(m2),
+    m3r=len(m3),m4r=len(m4), m5r=len(m5), m6r=len(m6), m7r=len(m7),m8r=len(m8), m9r=len(m9),ear=len(ea), har=len(ha),mor=len(mo))
 
 if __name__ == '__main__':
     app.run()
